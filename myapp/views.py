aa1d806983b40d9a4b3dc9624b2fe4e14b13e9cc
Manuel <manuels.gutierrezp@konradlorenz.edu.co>
2024-10-24 11:30:14 -0500

    new URL
    se añadio una nueva url "about" y una etiqueta html al hello world

--- myapp/views.py
+++ myapp/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("<h1> Hello world </h1>")
+
+def about (request):
+    return HttpResponse('About')
